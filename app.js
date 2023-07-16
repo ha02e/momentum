43f2ebfb3a1abf15be5fa55938e0da215a5aea84
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-07-16 22:08:21 +0900

    230716_변수 사용하기

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log(5645454);
 console.log("lalala");
+
+//console.log(5+2);
+//console.log(5*2);
+//console.log(5/2);
+
+//변수(Variables)
+const a=5;
+const b=2;
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
