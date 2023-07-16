c2b217a1446101e59077870f043b41dc651962c3
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-07-16 22:27:06 +0900

    230716_변수 만드는 법  const/let 차이점

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ console.log("lalala");
 //console.log(5*2);
 //console.log(5/2);
 
-//변수(Variables)
-const a=5;
+//변수(Variables) 
+const a=5;  //const(상수) : 생성 후 값이 바뀔 수 없다.
 const b=2;
+let myName="hayoung";  //let : 생성 후 값을 바꿀 수 있다. 재선언 X
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
+
+myName = "ha0";
+console.log("my name is "+myName);
