1485833b672208170ce30b4618cb0fa2d901acb6
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-08-05 23:44:09 +0900

    230805_자바스크립트 이벤트 학습

--- app.js
+++ app.js
@@ -188,5 +188,13 @@ title.innerText = "Hello!";
 function handleTitleClick(){  
     title.style.color = "blue";
 }
+function handleMouseEnter(){  
+    title.innerText = "Mouse is here!";
+}
+function handleMouseLeave(){  
+    title.innerText = "Mouse is gone!";
+}
 
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
