374edb3ab30d2d6092cfcadd16550188c38fb31b
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-08-07 13:51:09 +0900

    230807_toggle 함수

--- app.js
+++ app.js
@@ -189,13 +189,7 @@ h1.innerText = "Hello!";
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const clickedClass = "clicked";
-
-    if(h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    }else{
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
 }
 
 /*
