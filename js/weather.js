20ab76d6ad0f53e4f3e7dba060dc55ff1308fb0a
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-08-13 21:48:33 +0900

    230813_Weather API 활용해 날씨 기능 구현

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "cfa14e4abedc70a4e4fc69237b24c8bc";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("위도", lat, "경도", lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const weater = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weater.innerText = `${data.weather[0].main} / ${data.main.temp}도`;
+    });
 }
 function onGeoError(){
     alert("위치를 찾을 수 없습니다.");
